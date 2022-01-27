95e8c7fb1d2ad73b039d2c8650eb2e333af19e4b
joeac_vs <joeac@Y-EL-AASAR-LAPT>
2022-01-27 01:38:44 +0000

    found the reason for this project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # first_tester
 im just testing fjf
 ^^
+
+okay so i just relsied that im going to mkae a discord bot but i still have to figure out what its going to do
--- src/testing.py
+++ src/testing.py
@@ -1,7 +1,17 @@
-import pygame
+import discord
 
-pygame.init()
+client = discord.Client()
 
-print("hello world")
+@client.event
+async def on_ready():
+    print("logged in yay {0.user}".format(client))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('hello'):
+        await message.channel.send("hello to u")
+
+client.run("OTM2MDY4MDAxMzUzMDYwMzUy.YfHzQg.9YV_VlXogxiBoksZtuo4LoR0Wdg")

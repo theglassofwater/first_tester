38dee70dce28eeb940b3f085c628c14f89f0e094
joeac_vs <joeac@Y-EL-AASAR-LAPT>
2022-01-27 02:01:25 +0000

    .env isnt moving idk y

--- src/testing.py
+++ src/testing.py
@@ -1,10 +1,16 @@
 import discord
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("logged in yay {0.user}".format(client))
+
+
 
 @client.event
 async def on_message(message):
@@ -14,4 +20,4 @@ async def on_message(message):
     if message.content.startswith('hello'):
         await message.channel.send("hello to u")
 
-client.run("OTM2MDY4MDAxMzUzMDYwMzUy.YfHzQg.9YV_VlXogxiBoksZtuo4LoR0Wdg")
+client.run(os.getenv('TOKEN'))
